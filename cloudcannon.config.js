8f11c6c2de067ea2cf2011aaa5d8ee2134895269
Ayush Thakur <tsayush2390@gmail.com>
2023-06-21 18:11:25 +0000

    Updated 1 file via CloudCannon.

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -4,12 +4,4 @@ module.exports = {
     show_gutter:true,
     theme: "chrome"
   },
-  generator: {
-    metadata: {
-      markdown: "markdown-it",
-      "markdown-it": {
-        html: true,
-        linkify: true
-      }
-    }
 };
